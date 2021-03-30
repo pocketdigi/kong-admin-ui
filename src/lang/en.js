f8e653146e6f4bcadd17ca768167f07decd5104a
Daniel Adarve <dgonzalezad@gmail.com>
2021-03-30 15:27:19 +1100

    Updated english configuration

--- src/lang/en.js
+++ src/lang/en.js
@@ -6,20 +6,20 @@ export default {
         button:{
             enter:'Enter',
             clear:'Clear address',
-            notice1:'This app will not connect other servers except your kong admin api.Your kong admin api address is save in browser\'s localstorage, so it\'s safety to manage your kong.',
-            notice2:'All requests to Kong admin api is send by your browser,so make sure your device can access Kong admin api without any security auth.',
+            notice1:'This app will not connect with other servers except your kong admin api. Your kong admin api address is saved in your browser\'s localstorage, so it\'s safer to manage your kong.',
+            notice2:'All requests to the Kong admin api are sent by your browser, so make sure your device can access the Kong admin api without any security auth.',
             custom_header:'Custom headers(json)'
         },
         error:{
             urlEmpty:'Please enter your Kong admin api url',
-            urlError:'url error,must start with http:// or https://',
-            urlEndWithSlash:'url error,cant\'t end with /',
-            apiError:'Can\'t get your Kong\'s version,please check the address you input',
-            connectFail:'Failed to connect with your kong admin api,please check the address you input,or your computer has no permission to visit the admin api',
+            urlError:'url error, must start with http:// or https://',
+            urlEndWithSlash:'url error, cant\'t end with /',
+            apiError:'Can\'t get your Kong\'s version, please check the address you entered',
+            connectFail:'Failed to connect with your kong admin api, please check the address you entered, or your computer has no permission to visit the admin api',
             urlInvalid:'Kong admin api url error',
-            versionNotSupport:'Version {version} is not support,We only support 0.14.0 and higher',
-            header_format:'Header format error,must be json',
-            connect_fail:'Failed to connect with your kong admin api,please check the address you input,or your computer has no permission to visit the admin api'
+            versionNotSupport:'Version {version} is not supported, We only support 0.14.0 and higher',
+            header_format:'Header format error, must be json',
+            connect_fail:'Failed to connect with your kong admin api, please check the address you entered, or your computer has no permission to visit the admin api'
         }
     },
     common: {
